d76a9c3f2332d07fcf738cb076e505c70b0770d2
av3wagner <67894780+av3wagner@users.noreply.github.com>
2023-09-30 09:14:56 +0200

    Update and rename AWapp.py to app.py

---
+++ scr/app.py
@@ -0,0 +1,11 @@
+from dash import Dash, html
+
+Myapp = Dash(__name__)
+Myapp.layout = html.Div([
+    html.Div(children='Hello World')
+])
+server=Myapp.server
+#AWapp:server
+
+if __name__ == '__main__':
+    Myapp.run(debug=True)
