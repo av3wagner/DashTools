f63529b02f8fe341d131d8dc0cc13fe3969a1d00
av3wagner <67894780+av3wagner@users.noreply.github.com>
2023-09-30 09:04:34 +0200

    Update AWapp.py

--- scr/AWapp.py
+++ scr/AWapp.py
@@ -4,8 +4,8 @@ app = Dash(__name__)
 app.layout = html.Div([
     html.Div(children='Hello World')
 ])
-server=AWapp.server
-#AWapp:server
+#server=AWapp.server
+AWapp:server
 
 if __name__ == '__main__':
     app.run(debug=True)
