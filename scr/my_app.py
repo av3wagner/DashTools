95a6170a59a56cec63ea6085602eb4ad4a5c8905
av3wagner <67894780+av3wagner@users.noreply.github.com>
2023-10-03 08:44:36 +0200

    Update my_app.py

--- scr/my_app.py
+++ scr/my_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import sqlite3
 #import matplotlib.pyplot as plt
-import pyperclip
+#import pyperclip
 import dash
 from dash import dash_table
 from dash import dcc
